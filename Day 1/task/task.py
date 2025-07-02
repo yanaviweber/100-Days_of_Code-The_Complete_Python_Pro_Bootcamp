a6a744fd8a3b56a166e68e0e3da55c618b90dabb
Yana-Viktoriia Weber <creative.mashine@gmail.com>
2025-07-02 23:22:56 +0200

    First day study is completed: variables, input, output, concatenation, length of variable's value as text type, the first program about asking names of city and pet

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,4 @@
-# Write your code below this line 👇
-print("Hello, World!")
+print("Welcome to the Band Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet)
