da2c36213de6710fe6dc5c213854ab65f6d4ecb3
Yana-Viktoriia Weber <creative.mashine@gmail.com>
2025-07-03 23:51:49 +0200

    Topic: Types of Primitive Data Types. Begin of second day.

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,27 @@
-len("Hello")
+# Subcripting
+
+# print last symbol
+print("Hello"[4])
+print("Hello"[-1])
+
+# Sprint symbol, which is in 0 position
+print("Hello"[0])
+
+# Sprint symbol, which is in 01position
+print("Hello"[1])
+
+# String
+print("123" + "345") # Concatenation 2 strings
+
+# Integer = whole number
+print(123 + 345)
+
+# Large Integer
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
