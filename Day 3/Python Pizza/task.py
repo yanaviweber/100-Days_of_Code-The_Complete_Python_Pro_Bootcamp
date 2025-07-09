0ac69cc74d1a41f74f116fd66b4b967acbb059ce
Yana-Viktoriia Weber <creative.mashine@gmail.com>
2025-07-09 22:54:05 +0200

    Day 3: Fifth Lesson is finished - Python Pizza Delivery Program was written.--- TODO lines are corrected.

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,7 +3,7 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
-# todo: work out how much they need to pay on their size choice.
+# exercise - part 1: work out how much they need to pay on their size choice.
 bill = 0
 if size == "S":
     bill += 15
@@ -14,14 +14,14 @@ elif size == "L":
 else:
     print("You typed the wrong inputs.")
 
-# todo: work out how much to add to their bill based on their pepperoni choice.
+# exercise - part 2: work out how much to add to their bill based on their pepperoni choice.
 if pepperoni == "Y":
     if size == "S":
         bill += 2
     else:
         bill += 3
 
-# todo: work on their final amount based on whether if they want extra cheese.
+# exercise - part 3: work on their final amount based on whether if they want extra cheese.
 if extra_cheese == "Y":
     bill += 1
 
